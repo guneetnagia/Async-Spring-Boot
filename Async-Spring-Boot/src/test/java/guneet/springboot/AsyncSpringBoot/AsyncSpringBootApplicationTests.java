65d0c3665c8226b89a35e218a516da3899417f38
Guneet Singh <guneetnagia24@gmail.com>
2021-01-31 12:02:24 +0530

    project seed

---
+++ Async-Spring-Boot/src/test/java/guneet/springboot/AsyncSpringBoot/AsyncSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package guneet.springboot.AsyncSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsyncSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
